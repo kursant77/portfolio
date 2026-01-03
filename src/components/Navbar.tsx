0b3fab3d73035caf9c4404fee1a241b23d72f71b
Asadbek Jumanazarov <kursant410@gmail.com>
2026-01-03 17:28:07 +0500

    seo update

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -121,7 +121,7 @@ export default function Navbar() {
                   key={item.key}
                   whileHover={{ y: -2 }}
                   onClick={() => scrollToSection(item.href)}
-                  className={`px-3 py-2 text-[14px] xl:text-[15px] font-black transition-all duration-300 uppercase tracking-tight relative group whitespace-nowrap ${isActive
+                  className={`px-3 py-2 text-[14px] xl:text-[15px] font-black transition-colors duration-300 uppercase tracking-tight relative group whitespace-nowrap ${isActive
                     ? 'text-blue-600 dark:text-blue-400'
                     : 'text-gray-700 dark:text-gray-300 hover:text-blue-600 dark:hover:text-blue-400'
                     }`}
@@ -130,12 +130,12 @@ export default function Navbar() {
                   {isActive && (
                     <motion.div
                       layoutId="navUnderline"
-                      className="absolute -bottom-1 left-1/2 -translate-x-1/2 h-0.5 bg-blue-500 rounded-full shadow-[0_0_10px_rgba(37,99,235,0.8)] w-2/3"
-                      transition={{ type: "spring", bounce: 0.2, duration: 0.6 }}
+                      className="absolute -bottom-1 inset-x-0 mx-auto h-0.5 bg-blue-500 rounded-full shadow-[0_0_10px_rgba(37,99,235,0.8)] w-2/3"
+                      transition={{ type: "spring", bounce: 0.15, duration: 0.5 }}
                     />
                   )}
                   {!isActive && (
-                    <span className="absolute -bottom-1 left-1/2 -translate-x-1/2 h-0.5 bg-blue-500/50 transition-all duration-300 rounded-full w-0 group-hover:w-2/3" />
+                    <span className="absolute -bottom-1 inset-x-0 mx-auto h-0.5 bg-blue-500/50 transition-all duration-300 rounded-full w-0 group-hover:w-2/3" />
                   )}
                 </motion.button>
               );
